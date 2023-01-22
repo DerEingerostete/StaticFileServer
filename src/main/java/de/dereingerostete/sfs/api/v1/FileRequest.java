825a2a40d1bd08fbfbac5b4f6e5e08f217d90b0d
DerEingerostete <contact@dereingerostete.dev>
2023-01-22 14:08:22 +0100

    Added copyright header
    Took 6 minutes

--- src/main/java/de/dereingerostete/sfs/api/v1/FileRequest.java
+++ src/main/java/de/dereingerostete/sfs/api/v1/FileRequest.java
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) 2023 - DerEingerostete
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
+
 package de.dereingerostete.sfs.api.v1;
 
 import org.jetbrains.annotations.NotNull;
 
 public interface FileRequest {
-
 	@NotNull
 	String getFileName();
 
